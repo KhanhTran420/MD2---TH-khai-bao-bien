d292ccce483d2f2b2b7254b76498fdc470154822
unknown <khanhbi1998pt@gmail.com>
2022-02-08 11:22:47 +0700

    TH khai bao bien

---
+++ src/TH2.java
@@ -0,0 +1,18 @@
+public class TH2 {
+    public static void main(String[] args) {
+        // khai báo biến i là kiểu int có giá trị là 10
+        int i = 10;
+        // Khai báo biến f có kiểu là float có giá trị là 20.5
+        float f = 20.5f;
+        double d = 20.5;
+        boolean b = true;
+        char c = 'a';
+        String s = "Hà Nội";
+        System.out.println("i = " + i);
+        System.out.println("f = " + f);
+        System.out.println("d = " + d);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("s = " + s);
+    }
+}
